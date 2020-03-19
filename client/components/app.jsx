cd31012d1936a05ced8f9a54ff75bd47271d6a0a
Julian <jthrow00@gmail.com>
2020-03-18 23:03:46 -0700

    Modified App to render Splash

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import IntroPages from './introPages';
+import Splash from './splash';
 
 export default class App extends React.Component {
   render() {
-    return <IntroPages />;
+    return <Splash />;
   }
 }
