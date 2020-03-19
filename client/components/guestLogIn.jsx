ddcbfee0df8b1600c2353479c181c9a247e78ffa
wayne-daniels <wa.daniels@hotmail.com>
2020-03-19 21:44:39 +0000

    revised font on logo

--- client/components/guestLogIn.jsx
+++ client/components/guestLogIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function GuestLogIn(props) {
+
   return (
     <div className='mx-auto vw-100 vh-100 d-flex flex-column text-white align-items-center justify-content-center gradient'>
       <div className='w-100 h-100 my-3'></div>
