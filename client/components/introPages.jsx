0e355720e9cba5f16e2fbcebcbd8d44213381a04
Julian <jthrow00@gmail.com>
2020-03-18 23:04:33 -0700

    Modified skeleton in IntroPages render to include 4 div boxes

--- client/components/introPages.jsx
+++ client/components/introPages.jsx
@@ -8,9 +8,9 @@ import Splash from './splash';
 export default class IntroPages extends React.Component {
   render() {
     return (
-      <div
-        className='mx-auto vw-100 vh-100 d-flex flex-column text-white align-items-center justify-content-center gradient'>
+      <div className='mx-auto vw-100 vh-100 d-flex flex-column text-white align-items-center justify-content-center gradient'>
         <div className='w-100 h-100 my-3'></div>
+        <div className='w-100 h-100 mb-3'></div>
         <div className='w-100 h-100 mb-3'></div>
         <div className='w-100 h-100 mb-3'></div>
       </div>
