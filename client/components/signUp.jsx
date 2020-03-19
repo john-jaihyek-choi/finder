bf839f668ad1da7899c8736b117d87c8244b7cdd
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-03-19 06:08:49 +0000

    styled and positioned buttons and text input

--- client/components/signUp.jsx
+++ client/components/signUp.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default class SignUp extends React.Component {
     render() {
-        
+        return (
+        <>
+            <div className='w-100 h-100 my-3'></div>
+            <div className='w-100 h-100 mb-3'>
+                <form className="d-flex flex-wrap justify-content-center w-100 h-50">
+                    <label htmlFor="signUpUserName" className="text-white font-weight-bold w-75 mt-4 ml-4">CREATE USERNAME</label>
+                    <input id="signUpUserName" type="text" className="text-white w-75 h-50 text-center"/>
+                </form>
+            </div>
+            <div className='w-100 h-100 mb-3 d-flex flex-wrap justify-content-center'>
+                <button type="submit" className="w-75 h-25 mt-4 font-weight-bold">SUBMIT</button>
+                <button type="reset" className="w-75 h-25 mb-4 font-weight-bold">CANCEL</button>
+            </div>
+        </>
+        )
     }
 }
