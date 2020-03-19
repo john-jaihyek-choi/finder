8b1eb076b2b66f8a6c7b6453a19356d083caf39c
Julian <jthrow00@gmail.com>
2020-03-18 21:56:45 -0700

    Added html skeleton for intro pages; imported necessary modules

--- client/components/introPages.jsx
+++ client/components/introPages.jsx
@@ -3,9 +3,17 @@ import LogIn from './logIn';
 import SignUp from './signUp';
 import Splash from './splash';
 
-//Julian's template for Intro Pages (this will serve as a template for Log-in, Sign-up, and Splash Message)
+// Julian's template for Intro Pages (this will serve as a template for Log-in, Sign-up, and Splash Message)
 export default class IntroPages extends React.Component {
-    render() {
-        
-    }
+  render() {
+    return (
+      <div
+        className='mx-auto d-flex flex-column text-white align-items-center justify-content-center gradient'
+        style={{ width: '100vw', height: '100vh' }}>
+        <div className='w-100 h-25 mb-3 bg-white'></div>
+        <div className='w-100 h-25 mb-3 bg-white'></div>
+        <div className='w-100 h-25 bg-white'></div>
+      </div>
+    );
+  }
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector("#root")
-)
+ReactDOM.render(<App />, document.getElementById('root'));
