a15d9051c643a0b36a484b0d45919089ccd906ad
Julian <jthrow00@gmail.com>
2020-03-19 19:45:31 -0700

    Modified CardStack render to display cards; changed button styling

--- client/components/cardStack.jsx
+++ client/components/cardStack.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class CardStack extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {restaurants: []};
+    this.state = { restaurants: [] };
   }
 
   getRestaurants() {
@@ -20,20 +20,20 @@ export default class CardStack extends React.Component {
     return (
       <div className='mx-auto vw-100 vh-100 d-flex flex-column align-items-center justify-content-center gradient'>
         <div className='w-100 h-100 my-3'>
-          <div className='h-25 d-flex justify-content-around'>
-            <button className='shadow'>PROFILES</button>
-            <div className='d-flex align-items-center shadow'>STACK</div>
-            <button className='shadow'>LIKED</button>
+          <div className='h-100 mt-4 d-flex align-items-start justify-content-around'>
+            <button type='button' className='btn btn-outline-light button-outline shadow'>PROFILES</button>
+            <button className='d-flex align-items-center btn btn-outline-light button-outline shadow'>STACK</button>
+            <button type='button' className='btn btn-outline-light button-outline shadow'>LIKED</button>
           </div>
         </div>
-        <div className='w-100 h-100 mb-3 d-flex flex-column align-items-center justify-content-start'></div>
-        <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
+        <div className='w-100 h-100 mb-3 d-flex justify-content-center align-items-center'>
+          <div className='w-75 card rounded' style={{ height: '450px'}}></div>
         </div>
         <div className='w-100 h-100 mb-3'>
-          <div className='d-flex justify-content-around'>
-            <button className='shadow'>X</button>
-            <button className='shadow'>REDO</button>
-            <button className='shadow'>O</button>
+          <div className='h-100 d-flex align-items-center justify-content-around'>
+            <button type='button' className='mr-5 btn btn-outline-light button-outline shadow'>X</button>
+            <button type='button' className='mr-5 btn btn-outline-light button-outline shadow'>REDO</button>
+            <button type='button' className='btn btn-outline-light button-outline shadow'>O</button>
           </div>
         </div>
       </div>
