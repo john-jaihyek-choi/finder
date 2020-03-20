a217715763dfe50b321998ba30fda5bc3c42e882
wayne-daniels <wa.daniels@hotmail.com>
2020-03-20 01:18:40 +0000

    Basic structure complete, backend issues pending

--- client/components/guestLogIn.jsx
+++ client/components/guestLogIn.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-export default function GuestLogIn(props) {
+export default class GuestLogIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.guestClick = this.guestClick.bind(this);
+  }
 
+  guestClick(event) {
+  event.preventDefault();
+  this.props.setView('splash');
+    fetch("/api/users", {
+      method: 'POST'
+      // headers: {"Content-Type": "application/json"}
+      // body: JSON.stringify()
+    })
+    .then(response => console.log(response))
+  }
+
+render() {
   return (
     <div className='mx-auto vw-100 vh-100 d-flex flex-column text-white align-items-center justify-content-center gradient'>
       <div className='w-100 h-100 my-3'></div>
@@ -11,11 +27,13 @@ export default function GuestLogIn(props) {
       <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
         <button
           type='button'
-          className='w-75 py-3 btn btn-outline-light button-outline font-weight-bold'>
+          className='w-75 py-3 btn btn-outline-light button-outline font-weight-bold'
+          onClick={this.guestClick}>
           CONTINUE AS GUEST
         </button>
       </div>
       <div className='w-100 h-100 mb-3'></div>
     </div>
   );
+ }
 }
