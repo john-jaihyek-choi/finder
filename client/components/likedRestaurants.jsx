dd7082a2d0dc9fffa14087db8c5317612e274678
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-03-20 10:18:55 +0000

    styled div for the cards of restaurants

--- client/components/likedRestaurants.jsx
+++ client/components/likedRestaurants.jsx
@@ -4,8 +4,8 @@ export default class LikedRestaurants extends React.Component {
     render () {
         return (
             <div className='mx-auto vw-100 vh-100 d-flex flex-column align-items-center justify-content-center'>
-            <div className='w-100 h-50 my-3'>
-                <div className='h-25 mt-4 d-flex align-items-start justify-content-around'>
+            <div className='w-100 sticky-top navTop' style={{ background: 'white' }}>
+                <div className='h-25 mt-2 d-flex align-items-start justify-content-around'>
                     <div className='d-flex align-items-center text-secondary'><i className='fas fa-utensils fa-2x'></i></div>
                     <div className='d-flex align-items-center text-pink'><i className='fas fa-heart fa-2x'></i></div>
                     <div className='d-flex align-items-center text-white'><i className='fas fa-heart fa-2x'></i></div>
@@ -15,8 +15,22 @@ export default class LikedRestaurants extends React.Component {
                   <h3 className='mx-auto text-secondary'>Reviewed</h3>
                 </div>
             </div>
-            <div className='w-100 h-100 mb-3'>
-              <div className='w-75 mx-auto d-flex flex-column align-items-center justify-content-center card rounded shadow' style={{ height: '450px' }}>
+            <div className='w-100 h-75'>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
+              </div>
+              <div className='w-100 my-1 d-flex flex-column align-items-center justify-content-center card rounded cardShadow' style={{ height: '150px' }}>
               </div>
             </div>
           </div>
