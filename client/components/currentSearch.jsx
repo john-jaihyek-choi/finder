f6ce2e819e2f2f343ffd29f2c76a49faeb5ca062
wayne-daniels <wa.daniels@hotmail.com>
2020-03-21 00:32:45 +0000

    Completed User Can See Current Search feature

--- client/components/currentSearch.jsx
+++ client/components/currentSearch.jsx
@@ -3,27 +3,48 @@ import React from 'react';
 export default class CurrentSearch extends React.Component {
   constructor(props) {
     super(props);
-    // this.currentQuery = this.currentQuery.bind(this);
+    this.state = { food: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.currentQuery = '';
   }
 
-  // guestClick(event) {
-  //   event.preventDefault();
-  //   this.props.setView(' ');
+  handleChange(event) {
+    this.setState({ food: event.target.value })
+  }
 
-  // }
+  handleClick(event) {
+    event.preventDefault();
+    this.currentQuery = this.state.food;
+    this.setState({ food:''});
+}
 
   render() {
     return (
-      <div className="container display-flex">
-        <div className="row display-flex justify-content-left">
-          <i className="fas fa-arrow-left fa-3x gray mx-3 my-3"></i>
-        </div>
-        <div className="column w-80 justify-content-flex-start">
-          <div className="row justify-content-left">
-            <h4 className="pink justify-content-left ml-3">Current Query</h4>
+      <div className="container column display-flex">
+        <div className="column w-90 my-3 display-flex">
+          <div className="display-flex justify-content-left">
+            <i className="fas fa-arrow-left fa-3x gray"></i>
           </div>
-          <div className="row justify-content-center">
-            <h4 className="search w-99 px-1 py-1 justify-content-center">dummy search type</h4>
+          <div className="my-5">
+            <div className="justify-content-left">
+              <h4 className="pink justify-content-left ml-3">Current Query</h4>
+            </div>
+            <div className="justify-content-center">
+              <h4 className="query w-95 px-1 py-2 justify-content-center">{this.currentQuery}</h4>
+            </div>
+            </div>
+          <div className="wrapper justify-content-center mt-5">
+            <i className="mag-glass fas fa-search fa-2x gray"></i>
+            <input className="search shadow w-100 px-1 py-2 justify-content-left" placeholder="Search"
+              value={this.state.food} onChange={this.handleChange}></input>
+          </div>
+          <div className="d-flex justify-content-center mt-2">
+            <button type="text" form="userSignUp" className="form-control shadow d-flex submit btn font-weight-bold"
+                    onClick={this.handleClick}>SUBMIT</button>
+          </div>
+          <div className="d-flex justify-content-center mt-5">
+            <h5>Or try one of our suggestions below:</h5>
           </div>
         </div>
       </div>
