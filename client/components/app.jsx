a123700035aa33dbb32173bc196464881fcbe5d9
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-03-21 00:24:52 +0000

    finalized the cards to be populated based on the length of the likedRestaurant array received from the server

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,13 +3,14 @@ import IntroPages from './introPages';
 import Splash from './splash';
 import CardStack from './cardStack';
 import GuestLogIn from './guestLogIn';
-import LikedRestaurants from './likedRestaurants'
+import LikedReviewedRestaurants from './likedReviewedRestaurants'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       view: "login",
+      userId: null,
       likedRestaurants: []
     }
     this.setView = this.setView.bind(this);
@@ -29,13 +30,19 @@ export default class App extends React.Component {
       headers: { 'Content-Type' : 'application/json' }
     })
       .then(result => result.json())
-      .then(newUser => console.log(newUser));
+      .then(newUser => {
+        console.log(newUser);
+      });
   }
 
   getLikedRestaurants() {
     fetch('/api/likedRestaurants')
       .then(result => result.json())
-      .then(likedRestaurants => console.log(likedRestaurants))
+      .then(likedRestaurantsArr => {
+        return this.setState({
+          likedRestaurants: likedRestaurantsArr
+        })  
+      })
   }
 
 
@@ -51,7 +58,7 @@ export default class App extends React.Component {
       return <CardStack setView={this.setView} getLikedRestaurants={this.getLikedRestaurants} />;
     }
     if (this.state.view === "likedRestaurants") {
-      return <LikedRestaurants getLikedRestaurants={this.getLikedRestaurants}/>;
+      return <LikedReviewedRestaurants getLikedRestaurants={this.getLikedRestaurants} likedRestaurantsArr={this.state.likedRestaurants}/>;
     }
   }
 }
