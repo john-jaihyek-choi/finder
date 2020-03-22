d6fa3a7f707faddb387a040024868f5d3d1c50e9
Julian <jthrow00@gmail.com>
2020-03-21 23:33:18 -0700

    Converted Details to class component; added Google maps API map in details; added componentDidMount, componentDidUpdate lifecycle methods; added mapIsReady state

--- client/components/details.jsx
+++ client/components/details.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import restaurantData from '../../database/restaurants.json';
+import apiKeys from '../../database/apiKeys.json';
 
-export default function Details(props) {
-  const calculateTime = (time) => {
+export default class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mapIsReady: false };
+    this.map = null;
+  }
+
+  componentDidMount() {
+    this.setState({ mapIsReady: true });
+  }
+
+  componentDidUpdate() {
+    if (this.state.mapIsReady) {
+      this.map = new window.google.maps.Map(document.getElementById('map'), {
+        center: { lat: 37.80587, lng: -122.42058 },
+        zoom: 21,
+      });
+    }
+  }
+
+  calculateTime(time) {
     let num = Number(time.slice(0, 2));
     let period = 'AM';
     if (num >= 12) {
@@ -11,51 +30,43 @@ export default function Details(props) {
     }
     if (num === 0) return `12:${time.slice(2, time.length)} ${period}`;
     return `${num.toString()}:${time.slice(2, time.length)} ${period}`;
-  };
+  }
 
-  const renderHours = () => {
+  renderHours() {
     const days = ['Mon','Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     return days.map((day, ind) => {
       let hours;
-      if (!props.restaurant.hours[0].open[ind]) hours = `${day}: Closed`;
-      else hours = `${day}: ${calculateTime(props.restaurant.hours[0].open[ind].start)} - ${calculateTime(props.restaurant.hours[0].open[ind].end)}`;
+      if (!this.props.restaurant.hours[0].open[ind]) hours = `${day}: Closed`;
+      else hours = `${day}: ${this.calculateTime(this.props.restaurant.hours[0].open[ind].start)} - ${this.calculateTime(this.props.restaurant.hours[0].open[ind].end)}`;
       return <div>{hours}</div>;
     });
-  };
+  }
 
-  return (
-      <div className='w-75 mx-auto d-flex flex-column align-items-center justify-content-center card rounded shadow' style={{ height: '550px' }}>
+  render() {
+    return (
+      <div className='w-75 mx-auto d-flex flex-column align-items-center justify-content-center card rounded shadow' style={{ height: '650px' }}>
         <div className='w-100 h-50'>
           <img
             className='rounded'
-            onClick={() => props.toCardStack()}
-            src={props.restaurant.storeImageUrl}
-            alt={props.restaurant.restaurantName}
+            onClick={() => this.props.toCardStack()}
+            src={this.props.restaurant.storeImageUrl}
+            alt={this.props.restaurant.restaurantName}
             style={{ objectFit: 'cover', objectPosition: 'center bottom', height: '200px', width: '100%' }} />
         </div>
-        <div className='w-100 h-25 text-center text-pink d-flex align-items-center justify-content-center'>
-          <div className='w-50'>{props.rating}</div> |
-          <div className='w-50'>{props.price}</div>
+          <div className='w-100 h-25 text-center text-pink font-weight-bold d-flex align-items-center justify-content-center'>
+          <div className='w-100'>{this.props.rating}</div> |
+          <div className='w-100'>{this.props.price}</div> |
+          <div className='w-100'><i className="fas fa-map-marker-alt mr-2"></i>{(this.props.restaurant.distance * 0.000621371).toFixed(1)} mi</div>
         </div>
-        <div className='w-100 h-25 text-center text-pink font-weight-bold d-flex flex-column align-items-center justify-content-center'>
-          <div>{props.restaurant.restaurantName}</div>
-          <div>{props.restaurant.location.city}, {props.restaurant.location.state}</div>
-          <div><i className="fas fa-map-marker-alt mr-2"></i>{(props.restaurant.distance * 0.000621371).toFixed(1)} mi</div>
+        <div className='w-100 h-25 text-center text-pink font-weight-bold d-flex align-items-center justify-content-center'>
+          <div className='w-100'>{this.props.restaurant.restaurantName}</div> |
+          <div className='w-100'>{this.props.restaurant.location.city}, {this.props.restaurant.location.state}</div>
         </div>
-        <div className='w-100 h-100 text-center text-pink font-weight-bold d-flex flex-column align-items-center justify-content-center'>
-          {renderHours()}
+        <div className='w-100 h-75 mb-2 text-center text-pink font-weight-bold d-flex flex-column align-items-center justify-content-center details-text'>
+          {this.renderHours()}
         </div>
-        {/* <div className='w-100 h-100 text-center text-pink font-weight-bold d-flex align-items-center justify-content-center'>
-          <div className='w-100 h-100 text-center text-pink font-weight-bold d-flex flex-column align-items-center justify-content-center'>
-            <div>{props.restaurant.restaurantName}</div>
-            <div>{props.restaurant.location.city}, {props.restaurant.location.state}</div>
-            <div><i className="fas fa-map-marker-alt mr-2"></i>{(props.restaurant.distance * 0.000621371).toFixed(1)} mi</div>
-          </div>
-          <div className='w-50 h-100 text-center text-pink d-flex flex-column align-items-center justify-content-center'>
-            <div className='w-100'>{props.rating}</div>
-            <div className='w-100'>{props.price}</div>
-          </div>
-        </div> */}
+        <div className='w-100 h-75' id="map"></div>
       </div>
     );
+  }
 }
