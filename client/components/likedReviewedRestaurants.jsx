9a99d63faeea168765649c9009de04f675ea2cb4
Julian <jthrow00@gmail.com>
2020-03-24 16:55:48 -0700

    Moved Details component render to render method of LikedReviewedRestaurants from renderCard; added state properties of details and showDetails; added toDetails method to LikedReviewedRestaurants

--- client/components/likedReviewedRestaurants.jsx
+++ client/components/likedReviewedRestaurants.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import LikedReviewedCards from './LikedReviewedCards'
+import Details from './details';
 
 export default class LikedReviewedRestaurants extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { showDetails: false, details: null };
     this.toSwipePage = this.toSwipePage.bind(this)
     this.toReviewedPage = this.toReviewedPage.bind(this)
     this.toLikedPage = this.toLikedPage.bind(this)
+    this.toDetails = this.toDetails.bind(this)
   }
 
   toSwipePage(event) {
@@ -22,7 +25,20 @@ export default class LikedReviewedRestaurants extends React.Component {
     this.props.setView('reviewed')
   }
 
+  toDetails(restaurant) {
+    this.setState({ showDetails: true, details: restaurant });
+  }
+
   renderCard() {
+    let restaurantsArr = null
+    if (this.props.viewState === "likedRestaurants") {
+      restaurantsArr = this.props.likedRestaurantsArr
+    } else if (this.props.viewState === "reviewed") {
+      restaurantsArr = this.props.reviewedRestaurantsArr
+    }
+
+    // if (this.state.showDetails) return <Details price={price} rating={rating} restaurant={this.state.details} toCardStack={this.toCardStack} />;
+
     if (restaurantsArr < 1) {
       return (
         <div className='h-100 text-center mt-4 pt-4'>
@@ -49,12 +65,33 @@ export default class LikedReviewedRestaurants extends React.Component {
   }
 
   render() {
-    let restaurantsArr = null
-    if (this.props.viewState === "likedRestaurants") {
-      restaurantsArr = this.props.likedRestaurantsArr
-    } else if (this.props.viewState === "reviewed") {
-      restaurantsArr = this.props.reviewedRestaurantsArr
-    }
+    if (this.state.showDetails) return (
+      <div className='mx-auto vw-100 vh-100 d-flex flex-column align-items-center justify-content-center'>
+        <div className='w-100 h-100 my-3'>
+          <div className='h-100 mt-4 d-flex align-items-start justify-content-around'>
+            <div className='d-flex align-items-center text-white'><i className='fas fa-heart fa-2x'></i></div>
+            <div className='d-flex align-items-center text-pink'><i className='fas fa-heart fa-2x'></i></div>
+            <div className='d-flex align-items-center text-secondary'><i className='fas fa-heart fa-2x hover'></i></div>
+          </div>
+        </div>
+        <div className='w-100 h-100 mb-3'>
+          <Details restaurant={this.state.details} toPrevious={this.toPrevious} />;
+        </div>
+        <div className='w-100 h-100 mb-3'>
+          <div className='h-100 pb-4 d-flex align-items-end justify-content-around'>
+            {/* <button type='button' id='pass' className='stack-button pink btn button-outline shadow' onClick={this.handleClick}>
+              <i className='fas fa-times fa-lg'></i>
+            </button>
+            <button type='button' id='rewind' className='stack-button yellow btn button-outline shadow' onClick={this.handleClick}>
+              <i className='fas fa-undo fa-lg'></i>
+            </button>
+            <button type='button' id='like' className='stack-button green btn button-outline shadow' onClick={this.handleClick}>
+              <i className='fas fa-heart fa-lg'></i>
+            </button> */}
+          </div>
+        </div>
+      </div>
+    );
 
     return (
       <div className='mx-auto vw-100 vh-100 d-flex flex-column align-items-center justify-content-center'>
