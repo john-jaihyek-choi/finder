5be48a78005597b3ac247fcbd5c00463299d0f04
Julian <jthrow00@gmail.com>
2020-03-24 12:05:57 -0700

    Added guard clause to handleClick in CardStack to check if restaurants arr in state is truthy before enabling user to click on buttons

--- client/components/cardStack.jsx
+++ client/components/cardStack.jsx
@@ -53,6 +53,7 @@ export default class CardStack extends React.Component {
   }
 
   handleClick(e) {
+    if (!this.state.restaurants) return;
     if (e.currentTarget.id === 'like' && this.state.restaurants.length) return this.likeRestaurant(this.state.restaurants[this.state.index].yelpId, this.state.index);
     if (e.currentTarget.id === 'pass') return this.setState({ index: (this.state.index + 1) % this.state.restaurants.length, canRewind: true, showDetails: false });
     if (e.currentTarget.id === 'rewind' && this.state.canRewind) return this.setState({ index: (this.state.index + this.state.restaurants.length - 1) % this.state.restaurants.length, canRewind: false, showDetails: false });
