6ac2d08f0865d6707d664ef79ca1ee7b89a63ebf
Julian <jthrow00@gmail.com>
2020-03-24 12:53:57 -0700

    Moved dynamic appending of google maps API script to DOM to index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}`;
+document.body.appendChild(script);
+
 ReactDOM.render(<App />, document.getElementById('root'));
