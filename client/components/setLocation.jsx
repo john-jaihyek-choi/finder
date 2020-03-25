f683ced62c60b50af33da3289e83637cd432b24d
wayne-daniels <wa.daniels@hotmail.com>
2020-03-25 18:37:40 +0000

    begin work on slider component by adding dependencies

--- client/components/setLocation.jsx
+++ client/components/setLocation.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import RangeSlider from 'react-bootstrap-range-slider';
 
 export default class SetLocation extends React.Component {
   constructor(props) {
@@ -9,6 +11,19 @@ export default class SetLocation extends React.Component {
       currentQuery: this.props.currentQuery
     };
   }
+
+  // const slider = () => {
+
+  //   const [value, setValue] = useState(0);
+
+  //   return (
+  //     <RangeSlider
+  //       value={value}
+  //       onChange={changeEvent => setValue(changeEvent.target.value)}
+  //     />
+  //   );
+
+  // };
 
   render() {
     return (
