6fbdec25371d7daeaaeae92d93a7a248f27db33c
Judy <jiyc1119@gmail.com>
2020-03-26 12:34:21 -0700

    getLocation method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -178,7 +178,7 @@ export default class App extends React.Component {
       return <WriteReview setView={this.setView} from={this.from} postReview={this.postReview} reviewInfo={this.state.review}/>;
     }
     if(this.state.view === "userHomepage"){
-      return <UserHomepage />
+      return <UserHomepage setView={this.setView}/>
     }
   }
 }
--- client/components/userHomepage.jsx
+++ client/components/userHomepage.jsx
@@ -6,10 +6,20 @@ export default class UserHomepage extends React.Component {
   constructor(props) {
     super(props);
     this.toSwipePage = this.toSwipePage.bind(this)
+    this.swipeToSearch = this.swipeToSearch.bind(this)
+    this.getLocation = this.getLocation.bind(this)
   }
 
   toSwipePage(event) {
     this.props.setView('cardstack')
+  }
+
+  swipeToSearch(event){
+    this.props.setView('search')
+  }
+
+  getLocation(event){
+    this.props.setView('locationSettings')
   }
 
   render() {
@@ -23,31 +33,33 @@ export default class UserHomepage extends React.Component {
             <div className='d-flex align-items-center' onClick={this.toSwipePage}><i className='fas fa-utensils fa-2x hover gray'></i></div>
           </div>
         </div>
-        <div className="flex-row my-5 text-pink">
+        <div className="flex-row my-5 gray">
           <h1 className="title">FiNdEr_Tim</h1>
         </div>
+        <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
 
-        <div className='w-100 h-100 mb-3'>
-          <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
-            <div className="col-md d-flex align-items-center justify-content-center">
-              <button type='button' className='stack-button gray btn button-outline shadow m-4' onClick={this.handleClick}>
-                <i className='fas fa-map-marker-alt fa-2x'></i>
 
-                <p className='w-100 h-100 m-3'>LOCATION</p>
-              </button>
+            <div className="d-flex flex-column align-items-center">
+              <div>
+                <button type='button' className='stack-button gray btn button-outline shadow m-4 justify-content-center' onClick={this.getLocation}>
+                  <i className='fas fa-map-marker-alt fa-2x'></i>
+                </button>
+              </div>
+              <div>
+                <p className='d-flex flex-column m-2 gray font-weight-bold justify-content-center'>LOCATION</p>
+              </div>
             </div>
-
-            <div className="col-md d-flex align-items-center justify-content-center">
-              <button type='button' className='stack-button gray btn button-outline shadow m-4' onClick={this.handleClick}>
-                <i className='fas fa-search fa-2x'></i>
-
-                <p className='m-3 text-center'>SEARCH</p>
-              </button>
-            </div>
-
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <div>
+                <button type='button' className='stack-button gray btn button-outline shadow m-4 justify-content-center' onClick={this.swipeToSearch}>
+                  <i className='fas fa-search fa-2x'></i>
+                </button>
+              </div>
+              <div>
+                <p className='d-flex flex-column m-2 gray font-weight-bold justify-content-center'>SEARCH</p>
+              </div>
           </div>
         </div>
-
         <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
           <button
             type='button'
