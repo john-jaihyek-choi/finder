3699ee437dcb627fd5db107b615124bc0dfd0a33
Judy <jiyc1119@gmail.com>
2020-03-26 00:03:13 -0700

    logout button

--- client/components/userHomepage.jsx
+++ client/components/userHomepage.jsx
@@ -25,10 +25,17 @@ export default class UserHomepage extends React.Component {
           <h1>FiNdEr_Tim</h1>
         </div>
         <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
-          <div className='d-flex align-items-center'><i className='fas fa-user-alt fa-2x text-pink'></i></div>
-          <div className='d-flex align-items-center' id='likedRes' onClick={this.toSwipePage}><i className='fas fa-utensils fa-2x hover gray'></i></div>
+          <div className='d-flex align-items-center'><i className='fas fa-map-marker-alt hover gray'></i></div>
+          <div className='d-flex align-items-center'><i className='fas fa-search fa-2x hover gray'></i></div>
         </div>
-        <div className='w-100 h-100 mb-3'></div>
+        <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
+          <button
+            type='button'
+            className='w-75 btn btn-outline-light button-outline text-pink'
+            onClick={() => props.setView('userHomepage')}>
+            LOGOUT
+        </button>
+        </div>
 
       </div>
     )
