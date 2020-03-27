4ff2ca05ae8932ada648024a7ff7a8061538b79c
Julian <jthrow00@gmail.com>
2020-03-26 23:20:55 -0700

    Changed UserHomepage to display the current user name

--- client/components/userHomepage.jsx
+++ client/components/userHomepage.jsx
@@ -73,9 +73,9 @@ export default class UserHomepage extends React.Component {
         </div>
       </div>
       <div className="flex-row my-5 gray">
-        <h1 className="title">FiNdEr_Tim</h1>
+        <h1 className="title">{this.props.userInfo.userName}</h1>
       </div>
-      <div className='w-100 h-100 mb-3 d-flex align-items-center justify-content-center'>
+      <div className='w-100 vh-100 mb-3 d-flex align-items-center justify-content-center'>
         <div className="col-md d-flex flex-column align-items-center">
           <div>
             <button type='button' className='stack-button gray btn button-outline shadow m-4 align-items-center' onClick={this.getLocation}>
