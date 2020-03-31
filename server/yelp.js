2a1c6e60ec81b5d6609e7305025bde037c0557c5
Julian <jthrow00@gmail.com>
2020-03-31 16:19:06 -0700

    Added radius parameter to searchAllRestaurants fetch url

--- server/yelp.js
+++ server/yelp.js
@@ -3,13 +3,9 @@ const apiKey = "Bearer " + process.env.YELP_API_KEY;
 
 function searchAllRestaurants(lat, long, term, location, radius) {
   return fetch((location
-<<<<<<< HEAD
       ? `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&radius=${radius}&limit=30`
     : `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${long}&radius=${radius}&term=${term}&limit=30`), {
-=======
-    ? `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&radius=${radius}&limit=30`
-    : `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${long}&term=${term}&limit=30`), {
->>>>>>> e93a58b7dbf32478b0969c920ee11e66e5589101
+
     headers: {
       'Authorization': apiKey
     }
