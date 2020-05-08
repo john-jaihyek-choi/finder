229c42fdae1d228197d726b5753bef59265ca311
wayne-daniels <wa.daniels@hotmail.com>
2020-05-08 00:26:08 +0000

    Return statement added to line 65 of index.js

--- server/index.js
+++ server/index.js
@@ -62,8 +62,7 @@ app.get('/api/users', (req, res, next) => {
     .then(result => {
       const users = result.rows;
       if (!result) {
-        res.status(404).json({ error: 'Cannot be found' })
-        return
+        return res.status(404).json({ error: 'Cannot be found' })
       }
       res.status(200).json(users)
     })
